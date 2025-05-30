11d9a8b16c89dab4ba1356ddc37341f2d6cfbc33
Andrea Ostuni <andrea.ostuni@polito.it>
2025-05-30 09:17:47 +0000

    Add cost translation table and update obstacle threshold; improve logging levels

--- obstacle_distance/src/obstacle_distance.cpp
+++ obstacle_distance/src/obstacle_distance.cpp
@@ -6,6 +6,7 @@ using namespace std::chrono_literals;
 using std::placeholders::_1;
 using std::placeholders::_2;
 
+char * ObstacleDistance::cost_translation_table_ = NULL;
 /**
  * @brief Construct a new Obstacle Distance:: Obstacle Distance object
  *
@@ -27,7 +28,7 @@ ObstacleDistance::ObstacleDistance() : Node("obstacle_distance") {
   //      and publishes the created ObstacleDistance in the topic
   //      /obstacle_distance
   int mode = this->declare_parameter<int>("mode", 1);
-  obs_thresh_ = this->declare_parameter<int>("obstacle_threshold", 70);
+  obs_thresh_ = this->declare_parameter<int>("obstacle_threshold", 100);
   distMap_initialized_ = false;
 
   og_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>(
@@ -65,7 +66,7 @@ ObstacleDistance::ObstacleDistance() : Node("obstacle_distance") {
 
     obs_pub_ =
         this->create_publisher<obstacle_distance_msgs::msg::ObstacleDistance>(
-            "obstacle_distance", 1);
+            "obstacle_distance", rclcpp::QoS(rclcpp::KeepLast(1)).transient_local().reliable());
 
     RCLCPP_INFO(this->get_logger(),
                 "Mode %i. subscribed to topic /%s\nand publishing in topics "
@@ -122,8 +123,7 @@ void ObstacleDistance::obstacleDistanceService(
  */
 bool ObstacleDistance::staticObstacleDistanceService(
     const std::shared_ptr<
-        obstacle_distance_msgs::srv::GetStaticObstacleDistance::Request>
-        request,
+        obstacle_distance_msgs::srv::GetStaticObstacleDistance::Request>,
     std::shared_ptr<
         obstacle_distance_msgs::srv::GetStaticObstacleDistance::Response>
         response) {
@@ -142,7 +142,7 @@ bool ObstacleDistance::staticObstacleDistanceService(
 
 void ObstacleDistance::gridCallback(
     const nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
-  RCLCPP_INFO(this->get_logger(), "Costmap received!");
+  RCLCPP_DEBUG(this->get_logger(), "Costmap received!");
   nav_msgs::msg::OccupancyGrid vi;
   nav_msgs::msg::OccupancyGrid og;
   og.header = msg->header;
@@ -158,7 +158,7 @@ void ObstacleDistance::gridCallback(
       std::chrono::system_clock::now();
   auto millisecs =
       std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-  RCLCPP_INFO(this->get_logger(),
+  RCLCPP_DEBUG(this->get_logger(),
               "Time to compute obstacle dist: %.4f millisecs",
               (float)millisecs);
 
@@ -180,17 +180,16 @@ void ObstacleDistance::getStaticMap() {
     }
     RCLCPP_INFO(this->get_logger(), "service not available, waiting again...");
   }
-  RCLCPP_INFO(this->get_logger(), "Geting static map...");
+  RCLCPP_DEBUG(this->get_logger(), "Geting static map...");
   // Call the service
   auto request = std::make_shared<nav_msgs::srv::GetMap::Request>();
   auto result = static_map_client_->async_send_request(request);
-  // std::chrono::duration<int, std::milli> ms(200);
-  // if (result.wait_for(ms) == std::future_status::ready) {}
+  
   // Wait for the result.
   if (rclcpp::spin_until_future_complete(this->get_node_base_interface(),
                                          result) ==
       rclcpp::FutureReturnCode::SUCCESS) {
-    RCLCPP_INFO(this->get_logger(), "Map received!");
+    RCLCPP_DEBUG(this->get_logger(), "Map received!");
     nav_msgs::msg::OccupancyGrid vis;
     obstacle_distance_msgs::msg::ObstacleDistance od =
         compute_obstacle_distance(result.get()->map, vis);
@@ -211,57 +210,30 @@ void ObstacleDistance::look_label_in_distance(const cv::Mat &labels, int label,
                                               int width, int height,
                                               unsigned int &index,
                                               float &dist) {
+                                                
+  static const std::vector<std::pair<int, int>> directions = {
+      {-celldist, 0}, {celldist, 0}, {0, -celldist}, {0, celldist}};
 
-  // TODO: this method need to be reviewed and improved
   int closest_index = -1;
-  float min_dist = 9999999.0;
-  int fr = row - celldist;
-  // if (row < 2 and col < 2)
-  //   std::cout << "fixed row: " << fr;
-  if (fr >= 0) {
-    for (int c = col - celldist; c <= col + celldist && c < width; c++) {
-      // if (row < 2 and col < 2)
-      //   std::cout << " column: " << c << std::endl;
-      if (c >= 0 && labels.at<int>(c, fr) == label &&
-          distMap_.at<float>(c, fr) < min_dist) {
-        min_dist = distMap_.at<float>(c, fr);
-        closest_index = fr * width + c;
-        // if (closest_index < 0) {
-        //   std::cout << "closestidx:" << closest_index << " row:" << fr
-        //             << " col:" << c << " w:" << width << std::endl;
-        // }
-      }
-    }
-  }
-  fr = row + celldist;
-  if (fr < height) {
-    for (int c = col - celldist; c <= col + celldist && c < width; c++) {
-      if (c >= 0 && labels.at<int>(c, fr) == label &&
-          distMap_.at<float>(c, fr) < min_dist) {
-        min_dist = distMap_.at<float>(c, fr);
-        closest_index = fr * width + c;
-      }
-    }
-  }
-  int fc = col - celldist;
-  if (fc >= 0) {
-    for (unsigned int r = row - celldist; r <= row + celldist && r < height;
-         r++) {
-      if (r >= 0 && labels.at<int>(fc, r) == label &&
-          distMap_.at<float>(fc, r) < min_dist) {
-        min_dist = distMap_.at<float>(fc, r);
-        closest_index = r * width + fc;
-      }
-    }
-  }
-  fc = col + celldist;
-  if (fc < width) {
-    for (unsigned int r = row - celldist; r <= row + celldist && r < height;
-         r++) {
-      if (r >= 0 && labels.at<int>(fc, r) == label &&
-          distMap_.at<float>(fc, r) < min_dist) {
-        min_dist = distMap_.at<float>(fc, r);
-        closest_index = r * width + fc;
+  float min_dist = std::numeric_limits<float>::max();
+
+  for (const auto &[dr, dc] : directions) {
+    int r = row + dr;
+    int c = col + dc;
+
+    if (r >= 0 && r < height && c >= 0 && c < width) {
+      for (int d = -celldist; d <= celldist; ++d) {
+        int nr = (dr == 0) ? r : r + d;
+        int nc = (dc == 0) ? c : c + d;
+
+        if (nr >= 0 && nr < height && nc >= 0 && nc < width) {
+          if (labels.at<int>(nc, nr) == label && distMap_.at<float>(nc, nr) < 
+              min_dist) {
+              min_dist = distMap_.at<float>(nc, nr);
+              closest_index = nr * width + nc;
+            
+          }
+        }
       }
     }
   }
@@ -273,13 +245,13 @@ obstacle_distance_msgs::msg::ObstacleDistance
 ObstacleDistance::compute_obstacle_distance(
     const nav_msgs::msg::OccupancyGrid &og, nav_msgs::msg::OccupancyGrid &vis) {
 
+  // define the obstacle distance message
   obstacle_distance_msgs::msg::ObstacleDistance od;
-  // std::chrono::time_point<std::chrono::system_clock> t1 =
-  //     std::chrono::system_clock::now();
   od.header = og.header;
   od.header.stamp = this->get_clock()->now();
   od.info = og.info;
 
+  // fill the visualization message (for rviz)
   vis.header = og.header;
   vis.header.stamp = od.header.stamp;
   vis.info = og.info;
@@ -287,22 +259,20 @@ ObstacleDistance::compute_obstacle_distance(
   // allocate map structs so that x/y in the world correspond to x/y in the
   // image
   // (=> cv::Mat is rotated by 90 deg, because it's row-major!)
-  cv::Mat binaryMap = cv::Mat(og.info.width, og.info.height, CV_8UC1);
+  cv::Mat binaryMap = cv::Mat(og.info.width, og.info.height, CV_8UC1); // 8-bit single channel (grayscale) image
   distMap_.release();
-  distMap_ = cv::Mat(binaryMap.size(), CV_32FC1);
-  cv::Mat visualizationMap = cv::Mat(binaryMap.size(), CV_8UC1);
-  cv::Mat labels = cv::Mat(binaryMap.size(), CV_32SC1); // CV_16UC1);
+  distMap_ = cv::Mat(binaryMap.size(), CV_32FC1); // 32-bit float single channel image for distance map
+  cv::Mat visualizationMap = cv::Mat(binaryMap.size(), CV_8UC1); // 8-bit single channel (grayscale) image for visualization
+  cv::Mat labels = cv::Mat(binaryMap.size(), CV_32SC1); // 32-bit signed int single channel image for labels
 
-  std::vector<signed char>::const_iterator mapDataIter = og.data.begin();
-
-  unsigned char map_occ_thresh = (unsigned char)obs_thresh_;
+  std::vector<signed char>::const_iterator mapDataIter = og.data.begin(); // iterator for the occupancy grid data
+  unsigned char map_occ_thresh = (unsigned char)obs_thresh_; // threshold for occupied cells
 
   // iterate over map, store in image
   // (0,0) is lower left corner of OccupancyGrid
   for (unsigned int j = 0; j < og.info.height; ++j) {
     for (unsigned int i = 0; i < og.info.width; ++i) {
       if (*mapDataIter > map_occ_thresh) {
-        // m_mapInfo.height-1-i
         binaryMap.at<uchar>(i, j) = 0; // 0 -> black (occupied)
       } else {
         binaryMap.at<uchar>(i, j) = 255; // 255 -> white (free)
@@ -310,59 +280,42 @@ ObstacleDistance::compute_obstacle_distance(
       mapDataIter++;
     }
   }
-  // cv::imshow("binaryMap", binaryMap);
-  // cv::waitKey(0);
-
-  // std::chrono::time_point<std::chrono::system_clock> t2 =
-  //     std::chrono::system_clock::now();
-
-  // auto millisecs1 =
-  //     std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-  // RCLCPP_INFO(this->get_logger(), "Time prepare data: %.4f millisecs",
-  //             (float)millisecs1);
 
   cv::distanceTransform(binaryMap, distMap_, labels, cv::DIST_L2,
                         cv::DIST_MASK_PRECISE, cv::DIST_LABEL_PIXEL);
 
-  // std::chrono::time_point<std::chrono::system_clock> t3 =
-  //     std::chrono::system_clock::now();
-
-  // auto millisecs2 =
-  //     std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2).count();
-  // RCLCPP_INFO(this->get_logger(),
-  //             "Time to compute distance transform: %.4f millisecs",
-  //             (float)millisecs2);
-
   // Get a costmap for visualization
   cv::Mat cellDistMap = distMap_.clone();
   cv::Mat dst;
+  if (cost_translation_table_ == NULL) {
+    cost_translation_table_ = new char[256];
+
+    // special values:
+    cost_translation_table_[0] = 0;  // NO obstacle
+    cost_translation_table_[253] = 99;  // INSCRIBED obstacle
+    cost_translation_table_[254] = 100;  // LETHAL obstacle
+    cost_translation_table_[255] = -1;  // UNKNOWN
+
+    // regular cost values scale the range 1 to 252 (inclusive) to fit
+    // into 1 to 98 (inclusive).
+    for (int i = 1; i < 253; i++) {
+      cost_translation_table_[i] = static_cast<char>(1 + (97 * (i - 1)) / 251);
+    }
+  }
   cv::normalize(distMap_, dst, 0, 1, cv::NORM_MINMAX);
-  // cv::imshow("DistanceMap_norm", dst);
-  // cv::waitKey(0);
   for (unsigned int j = 0; j < og.info.height; ++j) {
     for (unsigned int i = 0; i < og.info.width; ++i) {
-      visualizationMap.at<unsigned char>(i, j) =
-          255 - static_cast<unsigned char>(dst.at<float>(i, j) * 255 + 0.5);
+      auto cost_value = 255 - static_cast<unsigned char>(
+          (dst.at<float>(i, j) * 255 + 0.5));
+      visualizationMap.at<unsigned char>(i, j) = 
+          cost_translation_table_[cost_value];
+      
       distMap_.at<float>(i, j) = distMap_.at<float>(i, j) * od.info.resolution;
-      // std::cout << "i: " << i << " j: " << j
-      //           << " cell_dist: " << cellDistMap.at<float>(i, j)
-      //           << " m_dist: " << distMap_.at<float>(i, j)
-      //           << " dist_norm: " << dst.at<float>(i, j)
-      //           << " vis: " << (int)visualizationMap.at<unsigned char>(i, j)
-      //           << std::endl;
     }
   }
 
   std::chrono::time_point<std::chrono::system_clock> t4 =
       std::chrono::system_clock::now();
-  // cv::imshow("VisualizationMap", visualizationMap);
-  // cv::waitKey(0);
-
-  // auto millisecs3 =
-  //     std::chrono::duration_cast<std::chrono::milliseconds>(t4 - t3).count();
-  // RCLCPP_INFO(this->get_logger(),
-  //             "Time to compute visualization map: %.4f millisecs",
-  //             (float)millisecs3);
 
   // compute labels
   int index = 0;
@@ -374,22 +327,13 @@ ObstacleDistance::compute_obstacle_distance(
         od.indexes.push_back(index);
       else {
         unsigned int final_idx;
-        float min_dist = 10000.0;
+        float min_dist = std::numeric_limits<float>::max();
         unsigned int idx;
-        if (i < 2 and j < 2)
-          std::cout << std::endl;
         for (int cdist = celldist - 1; cdist <= celldist + 1; cdist++) {
           if (cdist > 0) {
-            float dist = 10000.0;
+            float dist = std::numeric_limits<float>::max();
             look_label_in_distance(labels, labels.at<int>(i, j), cdist, j, i,
                                    od.info.width, od.info.height, idx, dist);
-            // if (i < 2 and j < 2) {
-            //   std::cout << "i: " << i << " j: " << j << " index: " << index
-            //             << " celldist: " << cdist << std::endl;
-            //   std::cout << "label(i,j): " << labels.at<int>(i, j)
-            //             << " label_idx: " << (int)idx << " mindist: " << dist
-            //             << std::endl;
-            // }
             if (dist < min_dist) {
               min_dist = dist;
               final_idx = idx;
@@ -404,31 +348,24 @@ ObstacleDistance::compute_obstacle_distance(
 
   std::chrono::time_point<std::chrono::system_clock> t5 =
       std::chrono::system_clock::now();
-  // // std::cout << "indexes:" << od.indexes << std::endl;
 
   auto millisecs4 =
       std::chrono::duration_cast<std::chrono::milliseconds>(t5 - t4).count();
 
-  RCLCPP_INFO(this->get_logger(), "Time to compute labels: %i millisecs",
+  RCLCPP_DEBUG(this->get_logger(), "Time to compute labels: %li millisecs",
               millisecs4);
 
   // distance map is in cells,
   // we transform to distance in meters
-  // distMap_ = distMap_ * od.info.resolution;
 
   // Transform back to array data
   vis.data.clear();
   for (unsigned int j = 0; j < od.info.height; ++j) {
     for (unsigned int i = 0; i < od.info.width; ++i) {
       od.distances.push_back(distMap_.at<float>(i, j));
-      // od.indexes.push_back(labels.at<int>(i, j));
       vis.data.push_back((int)visualizationMap.at<unsigned char>(i, j));
     }
   }
-  // RCLCPP_INFO(this->get_logger(),
-  //             "GridMap2D created with %d x %d cells at %f
-  //             resolution.", od.info.width, od.info.height,
-  //             od.info.resolution);
   return od;
 }
 
@@ -437,7 +374,6 @@ void ObstacleDistance::clickedPointCallback(
     const geometry_msgs::msg::PointStamped::SharedPtr msg) {
 
   mutex_.lock();
-  // if(distMap_initialized_)
   obstacle_distance_msgs::msg::ObstacleDistance od = obsDist_;
   mutex_.unlock();
 
@@ -460,34 +396,30 @@ void ObstacleDistance::clickedPointCallback(
 
   // index to cell
   if (ob_idx >= (od.info.width * od.info.height)) {
-    std::cout << "index " << ob_idx << " greater than map size!!!" << std::endl;
+    RCLCPP_WARN_STREAM(
+        this->get_logger(),
+        "Index " << ob_idx << " greater than map size!!! Setting to last cell");
     ob_idx = (od.info.width * od.info.height) - 1;
   }
-  // const div_t result = div(ob_idx, (int)od.info.width);
-  // xcol = result.rem;
-  // yrow = result.quot;
   unsigned int o_ycell = floor(ob_idx / od.info.width);
   unsigned int o_xcell = ob_idx % od.info.width;
 
   // cell to world point (obstacle)
   float x = o_xcell * od.info.resolution + od.info.origin.position.x;
   float y = o_ycell * od.info.resolution + od.info.origin.position.y;
-  // Eigen::Vector2d obstacle(x, y);
 
-  // vector between person and obstacle
-  // Eigen::Vector2d diff = apos - obstacle;
-
-  std::cout << std::endl;
-  std::cout << "Clicked point: " << std::endl;
-  std::cout << "x: " << msg->point.x << " y: " << msg->point.y << std::endl;
-  std::cout << "Cell x: " << xcell << " cell y: " << ycell << std::endl;
-  std::cout << "Index: " << index << " obstacle distance: " << dist
-            << std::endl;
-  std::cout << "Closest obstacle: " << std::endl;
-  std::cout << "Index: " << ob_idx << std::endl;
-  std::cout << "Cell xcell: " << o_xcell << " ycell: " << o_ycell << std::endl;
-  std::cout << "World point x: " << x << " y: " << y << std::endl;
-  std::cout << std::endl;
+  RCLCPP_DEBUG_STREAM(
+      this->get_logger(),
+      "Clicked point: x: " << msg->point.x << " y: " << msg->point.y
+                           << " Cell x: " << xcell << " cell y: " << ycell
+                           << " Index: " << index
+                           << " obstacle distance: " << dist
+                           << " Closest obstacle index: " << ob_idx
+                           << " Cell xcell: " << o_xcell
+                           << " ycell: " << o_ycell
+                           << " World point x: " << x
+                           << " y: " << y);
+  // Publish the clicked point and the closest obstacle
 
   visualization_msgs::msg::Marker m1;
   m1.header.frame_id = "map";
